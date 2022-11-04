2076969496df16f8506f6559f0b83c4e4914c0b1
saberlee1987 <saberazizi66@yahoo.com>
2022-11-04 23:25:40 +0330

    add PersonController and PersonService and PersonRepository

--- src/main/java/com/saber/spring_boot_web_demo/SpringBootWebDemoApplication.java
+++ src/main/java/com/saber/spring_boot_web_demo/SpringBootWebDemoApplication.java
@@ -3,7 +3,7 @@ package com.saber.spring_boot_web_demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.saber")
 public class SpringBootWebDemoApplication {
 
 	public static void main(String[] args) {
